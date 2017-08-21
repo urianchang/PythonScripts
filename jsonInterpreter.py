125f78d38835606cecaf660acae24dc747d03c51
Urian <uchang@sightmachine.com>
2017-08-21 13:52:54 -0700

    Cleaned up the script. Removed unnecessary comment.

--- jsonInterpreter.py
+++ jsonInterpreter.py
@@ -1,6 +1,6 @@
 '''
 What this script does:
-    1. Make an API GET request
+    1. Make an API GET request and pass the session cookie for authentication
     2. Parse response as JSON
     3. Pretty print the JSON
 '''
@@ -11,7 +11,6 @@ import json
 URL = "http://cs.stage.int.sightmachine.com/v1/datatab/cycle?asset_selection=%7B%22machine_source%22%3A%5B%22CS_QD_StockFit_1%22%5D%7D"
 cookie = {'session' : 'eyJfZnJlc2giOnRydWUsIl9pZCI6eyIgYiI6Ill6ZzNZVGMwTkRKaFltRXpOVGRpTVRrMVpXVmxORGRsWldVek9UQTBNams9In0sInVzZXJfaWQiOiI1NTVmZGNjMTc1NzNmMTZiOWNjOGI4OWMifQ.DHzTuA.GtvZRA6FzqJaJ-9Ngkusjvr8fOk'}
 
-# URL = "http://pokeapi.co/api/v2/pokemon/4/"
 r = requests.get(url=URL, cookies=cookie)
 jsonResponse = r.json()
 
