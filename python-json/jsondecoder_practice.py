ea408698668ee6d142992e47b9e3d476830f42c0
Urian <uchang@sightmachine.com>
2018-02-07 17:20:13 -0800

    Added example of multiline string decoding

--- python-json/jsondecoder_practice.py
+++ python-json/jsondecoder_practice.py
@@ -1,6 +1,18 @@
-import json
+# import json
+# import collections
+#
+# j = json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode('{"foo":1, "bar": 2}')
+#
+# print j
+
+import simplejson
 import collections
 
-j = json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode('{"foo":1, "bar": 2}')
+DECODER = simplejson.JSONDecoder(object_pairs_hook=collections.OrderedDict)
 
-print j
+print DECODER.decode(r"""
+{
+  "multiline": true,
+  "json": true
+}
+""")
