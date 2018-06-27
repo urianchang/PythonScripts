9c98699f557d88792f347c8bb497dd9eac425cf6
Urian <uchang@sightmachine.com>
2018-06-27 12:09:39 -0700

    Experimenting with localize and normalize

--- Python-2.7/pytz/basic1.py
+++ Python-2.7/pytz/basic1.py
@@ -1,4 +1,16 @@
+from datetime import datetime
+
 import pytz
 
-for tz in pytz.all_timezones:
-    print tz
+
+pst = pytz.timezone('US/Pacific')
+naive = datetime.now()
+sydney = datetime.now(tz=pytz.timezone('Australia/Sydney'))
+
+# print naive
+# print pst.localize(naive)
+# print pst.localize(sydney)  # ValueError: Not naive datetime
+# print naive.astimezone(pytz.UTC)    # ValueError: astimezone() can't be applied to naive datetime
+print pst.localize(naive).astimezone(pytz.UTC)
+print pytz.UTC.localize(naive)
+print pst.localize(naive).astimezone(pytz.UTC) == pytz.UTC.localize(naive)  # False
