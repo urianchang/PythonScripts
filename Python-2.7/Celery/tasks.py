2984a8797e84c8d782c257e2e7d59575c508c12c
Urian <uchang@sightmachine.com>
2018-08-14 22:48:43 -0700

    Added notes and getting started example from Celery docs

--- Python-2.7/Celery/tasks.py
+++ Python-2.7/Celery/tasks.py
@@ -2,7 +2,11 @@ from celery import Celery
 
 # 'tasks': name of the current module so name can be automatically generated in __main__ module
 # 'broker': URL of the message broker that you want to use
-app = Celery('tasks', broker = 'pyamqp://guest@localhost//')
+app = Celery(
+    'tasks',
+    backend='rpc://',
+    broker='pyamqp://guest@localhost//',
+)
 
 # 'amqp' - RabbitMQ  |  'redis' - Redis
 # Make sure RabbitMQ-Server is running before running the script
